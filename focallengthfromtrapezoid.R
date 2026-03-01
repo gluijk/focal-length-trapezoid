38ff1b41d1a9f0d34bdb58446b3c3728169f7e9d
Guillermo Luijk <45771557+gluijk@users.noreply.github.com>
2026-03-01 10:28:10 +0100

    Add files via upload

--- focallengthfromtrapezoid.R
+++ focallengthfromtrapezoid.R
@@ -54,17 +54,14 @@ get_aspectratio_focallength_FOV <- function(width, height, x, y) {
     
     # Plot of trapezoid for checking
     img=matrix(0, ncol=width, nrow=height)
-    
     draw_line <- function(M, x0, y0, x1, y1) {
         n <- max(abs(x1 - x0), abs(y1 - y0)) + 1
-        
         xs <- round(seq(x0, x1, length.out = n))
         ys <- round(seq(y0, y1, length.out = n))
-        
         M[cbind(ys, xs)] <- 1
         M
     }
-    
+    # Plot 3-pixel width lines
     for (dx in c(-1,0,1)) {
         for (dy in c(-1,0,1)) {
             img=draw_line(img, x[1]+dx, y[1]+dy, x[2]+dx, y[2]+dy)
@@ -80,7 +77,7 @@ get_aspectratio_focallength_FOV <- function(width, height, x, y) {
     # i.e. (m1x, m1y) are the cordinates of the first corner, (m2x, m2y) of the second corner and so on
     # m1, m2, m3, m4 follow the order: top-left, top-right, bottom-left, bottom-right
     # Let u0, v0 be the pixel coordinates of the principal point of the image
-    # which for a normal camera will be the center of the image: i.e. u0=IMAGEWIDTH/2, v0 =IMAGEHEIGHT/2
+    # which for a normal camera will be the centre of the image: i.e. u0=IMAGEWIDTH/2, v0 =IMAGEHEIGHT/2
     # This assumption does not hold if the image has been cropped asymmetrically
     
     # Transform the image so the principal point is at (0,0) which makes the following equations much easier
@@ -99,11 +96,9 @@ get_aspectratio_focallength_FOV <- function(width, height, x, y) {
     # Compute k2
     k2 <- ((m1y - m4y) * m3x - (m1x - m4x) * m3y + m1x * m4y - m1y * m4x) /
           ((m2y - m4y) * m3x - (m2x - m4x) * m3y + m2x * m4y - m2y * m4x)
-    
     # Compute k3
     k3 <- ((m1y - m4y) * m2x - (m1x - m4x) * m2y + m1x * m4y - m1y * m4x) /
           ((m3y - m4y) * m2x - (m3x - m4x) * m2y + m3x * m4y - m3y * m4x)
-    
     # Focal length in pixels
     focal_length_px_squared <- -((k3 * m3y - m1y) * (k2 * m2y - m1y) + (k3 * m3x - m1x) * (k2 * m2x - m1x)) /
                                 ((k3 - 1) * (k2 - 1))
@@ -138,6 +133,7 @@ get_aspectratio_focallength_FOV <- function(width, height, x, y) {
         # -------------------------------
         # Parallel lines are approximately parallel in image
         # Foreshortening negligible, no projective correction needed
+        cat("WARNING: affine / weak-perspective so aspect ratio derived from Euclidean distances, unstable focal length/FOV\n")
         whRatio <- sqrt(
             ((m2y - m1y)^2 + (m2x - m1x)^2) /  # observed width  (squared Euclidean distance between points m1 and m2)
             ((m3y - m1y)^2 + (m3x - m1x)^2)    # observed height (squared Euclidean distance between points m1 and m3)
@@ -195,7 +191,7 @@ get_aspectratio_focallength_FOV <- function(width, height, x, y) {
 
 
 ###################################################
-# EXAMPLES TO CHECK ALGORITHM
+# EXAMPLES
 
 # (x,y) coordinates expressed as: top-left, bottom-left, bottom-right, top-right
 
@@ -267,82 +263,7 @@ AR=get_aspectratio_focallength_FOV(width, height, x, y)  # 1.51 (1.5), 49.89mm (
 
 # REAL WORLD EXAMPLES
 
-# Fuji X-S10 con 16,5mm (eq.) → 16,84mm estimados
-width=6240; height=4160
-x=c(1516,  982, 6181, 5254)
-y=c(2391, 3609, 3372, 1873)
-AR=get_aspectratio_focallength_FOV(width, height, x, y)
-
-
-# 'Lunch atop a Skyscraper' → 43,92mm estimados
-width=1920; height=1481
-x=c(55,  64, 1774, 1783)
-y=c(635, 734, 912, 780)
-AR=get_aspectratio_focallength_FOV(width, height, x, y)
-
-
-# Canon R5 II con 85mm → 82,80mm estimados
-width=1920; height=1281
-x=c(190,  150, 1049, 1077)
-y=c(550, 1173, 1148, 283)
-AR=get_aspectratio_focallength_FOV(width, height, x, y)
-
-
-# Sony A7R V con 100mm → 95,87mm estimados
-width=1920; height=1280
-x=c(959,  958, 1595, 1581)
-y=c(734, 1001, 1273, 1030)
-AR=get_aspectratio_focallength_FOV(width, height, x, y)
-
-
-# Ricoh GR IV con 28mm (eq.) → 28,26mm estimados
-width=1280; height=1920
-x=c(557,  80, 1028, 819)
-y=c(493, 1826, 1525, 322)
-AR=get_aspectratio_focallength_FOV(width, height, x, y)
-
-
-# Sony A7R V con 200mm → 169,19mm estimados
-width=4000; height=2667
-x=c(3468, 3475, 3926, 3914)
-y=c( 569, 1255, 1470,  804)
-AR=get_aspectratio_focallength_FOV(width, height, x, y)
-
-
-# Nikon Z8 con 45mm → 46,51mm estimados
-width=1920; height=1280
-x=c(766,  752, 1395, 1376)
-y=c(209, 684, 978, 652)
-AR=get_aspectratio_focallength_FOV(width, height, x, y)
-
-
-# Quake III Arena LEFT → 19,04mm estimados
-width=2560; height=1440
-x=c(348,  247, 736, 777)
-y=c(475, 1160, 1111, 595)
-AR=get_aspectratio_focallength_FOV(width, height, x, y)
-
-# Quake III Arena RIGHT → 19,06mm estimados
-width=2560; height=1440
-x=c(1785,  1815, 2399, 2323)
-y=c(750, 1149, 1190, 703)
-AR=get_aspectratio_focallength_FOV(width, height, x, y)
-
-
-# Escher LEFT → 30,33mm estimados
-width=1159; height=1096
-x=c(202,  60, 608, 608)
-y=c(386, 945, 1035, 576)
-AR=get_aspectratio_focallength_FOV(width, height, x, y)
-
-# Escher - RIGHT -> 31.34mm estimados
-width=1159; height=1096
-x=c(608,  608, 1155, 952)
-y=c(271, 912, 773, 30)
-AR=get_aspectratio_focallength_FOV(width, height, x, y)
-
-
-# Panasonic S1R con 14-28mm a 14mm → 14,60mm estimados
+# Panasonic S1R con 14-28mm a 14mm → 14,60mm estimados (several rectangles)
 width=1920; height=1281
 x=c(1207,  1158, 1786, 1684)
 y=c(585, 775, 811, 554)
@@ -372,3 +293,79 @@ width=1920; height=1281
 x=c( 682,  554,  638,  745)
 y=c(1024, 1231, 1277, 1079)
 AR=get_aspectratio_focallength_FOV(width, height, x, y)  # 15.06mm
+
+
+# Fuji X-S10 con 16,5mm (eq.) → 16,84mm estimados
+width=6240; height=4160
+x=c(1516,  982, 6181, 5254)
+y=c(2391, 3609, 3372, 1873)
+AR=get_aspectratio_focallength_FOV(width, height, x, y)
+
+
+# Ricoh GR IV con 28mm (eq.) → 28,26mm estimados
+width=1280; height=1920
+x=c(557,  80, 1028, 819)
+y=c(493, 1826, 1525, 322)
+AR=get_aspectratio_focallength_FOV(width, height, x, y)
+
+
+# Nikon Z8 con 45mm → 46,51mm estimados
+width=1920; height=1280
+x=c(766,  752, 1395, 1376)
+y=c(209, 684, 978, 652)
+AR=get_aspectratio_focallength_FOV(width, height, x, y)
+
+
+# Canon R5 II con 85mm → 82,80mm estimados
+width=1920; height=1281
+x=c(190,  150, 1049, 1077)
+y=c(550, 1173, 1148, 283)
+AR=get_aspectratio_focallength_FOV(width, height, x, y)
+
+
+# Sony A7R V con 100mm → 95,87mm estimados
+width=1920; height=1280
+x=c(959,  958, 1595, 1581)
+y=c(734, 1001, 1273, 1030)
+AR=get_aspectratio_focallength_FOV(width, height, x, y)
+
+
+# Sony A7R V con 200mm → 169,19mm estimados
+width=4000; height=2667
+x=c(3468, 3475, 3926, 3914)
+y=c( 569, 1255, 1470,  804)
+AR=get_aspectratio_focallength_FOV(width, height, x, y)
+
+
+# 'Lunch atop a Skyscraper' → 43,92mm estimados
+width=1920; height=1481
+x=c(55,  64, 1774, 1783)
+y=c(635, 734, 912, 780)
+AR=get_aspectratio_focallength_FOV(width, height, x, y)
+
+
+# Quake III Arena LEFT → 19,04mm estimados
+width=2560; height=1440
+x=c(348,  247, 736, 777)
+y=c(475, 1160, 1111, 595)
+AR=get_aspectratio_focallength_FOV(width, height, x, y)
+
+# Quake III Arena RIGHT → 19,06mm estimados
+width=2560; height=1440
+x=c(1785,  1815, 2399, 2323)
+y=c(750, 1149, 1190, 703)
+AR=get_aspectratio_focallength_FOV(width, height, x, y)
+
+
+# Escher LEFT → 30,33mm estimados
+width=1159; height=1096
+x=c(202,  60, 608, 608)
+y=c(386, 945, 1035, 576)
+AR=get_aspectratio_focallength_FOV(width, height, x, y)
+
+# Escher - RIGHT -> 31.34mm estimados
+width=1159; height=1096
+x=c(608,  608, 1155, 952)
+y=c(271, 912, 773, 30)
+AR=get_aspectratio_focallength_FOV(width, height, x, y)
+
